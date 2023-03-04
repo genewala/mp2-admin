d02fbdbd9a6c75c9cdd6aec3b741615213980a5a
unknown <demoneyesrhada@gmail.com>
2023-03-04 13:42:01 +0800

    third commit

--- src/components/DataPlane/ControlPanel/Statistics.jsx
+++ src/components/DataPlane/ControlPanel/Statistics.jsx
@@ -10,7 +10,24 @@ const Statistics = () => {
         <DataBackground />
       </div>
       <div className='main-stat'>
-      Stat
+      <div id='dashboard'>
+      <div id='dashboard-left' >
+        <div id='dashboard-left-top'>
+          daily sent messages
+        </div>
+        <div id='dashboard-left-bottom'>
+          total sent messages
+        </div>
+      </div>
+      <div id='dashboard-right'>
+        <div id='dashboard-right-top'>
+          daily deleted message
+        </div>
+        <div id='dashboard-right-bottom'>
+          total deleted message
+        </div>
+      </div>
+      </div>
     </div>
 
     </div>
